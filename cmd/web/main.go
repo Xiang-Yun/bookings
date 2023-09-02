cfedf35a514d2c74db3f406b008d2f9463f6ff14
Xiang-Yun <david.deng0822@gmail.com>
2023-09-02 12:44:07 +0800

    add session / config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,15 +7,32 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // main is the main function
 func main() {
-	var app config.AppConfig
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = time.Duration(24) * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
 	tc, err := render.CreateTemplateCache()
-	fmt.Println(tc)
+
 	if err != nil {
 		log.Fatal(err)
 	}
